574affbd091d12d632090134612df7f8fdb09f75
jashwanth <jashwanth328910@gmail.com>
2022-12-06 12:32:26 +0530

    adding homepage get req

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const db = require("./models/index");
 const app = require("./app");
+const apiEndPoints = require("./ecommerce.postman_collection.json");
 
 db.sequelize.sync({ alter: true }).then(() => {
   console.log("tables created successfully");
@@ -9,6 +10,13 @@ require("./routes/category.route")(app);
 require("./routes/product.route")(app);
 require("./routes/auth.route")(app);
 require("./routes/cart.route")(app);
+
+app.get("/", (req, res) => {
+  apiEndPoints["message"] = "postman api collection";
+  res.setHeader("content-type", "application/json");
+  res.writeHead(200);
+  res.end(JSON.stringify(apiEndPoints));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on PORT: ${process.env.PORT}`);
